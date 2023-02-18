6d62d5045b5ed45131802f47885c4060a95b43e7
VIJAY235MURUGAN <infigonking@gmail.com>
2023-02-18 20:24:23 +0530

    first commit

---
+++ src/app/user.ts
@@ -0,0 +1,13 @@
+export class User {
+}
+export interface User {
+    id: number,
+    name: string,
+    email: string,
+    mobile: string,
+    gender: string,
+    dob: Date,
+    isActive: boolean,
+    range?: any
+    userType?: string
+}
